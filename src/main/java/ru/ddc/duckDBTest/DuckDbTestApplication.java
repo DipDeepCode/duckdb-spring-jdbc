d03da2968a4c513136a9aa07765027a1a7d6071e
Sergey Kovalev <dipdeepcode@mail.ru>
2023-05-23 00:19:45 +0300

    init commit

---
+++ src/main/java/ru/ddc/duckDBTest/DuckDbTestApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.duckDBTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckDbTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ddc/duckDBTest/DuckDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.duckDBTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckDbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
